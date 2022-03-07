9730f5a3d2a57867cd9d53c80d8d8a0b42be873a
Taneya <twil609@aucklanduni.ac.nz>
2022-03-07 20:02:38 +1000

    Activity started

--- src/ictgradschool/industry/oop/truncateamount/TruncateAmount.java
+++ src/ictgradschool/industry/oop/truncateamount/TruncateAmount.java
@@ -1,5 +1,7 @@
 package ictgradschool.industry.oop.truncateamount;
 
+import ictgradschool.Keyboard;
+
 /**
  * Write a program that prompts the user to enter an amount and a number of decimal places.  The program should then
  * truncate the amount to the user-specified number of decimal places using String methods.
@@ -20,15 +22,33 @@ public class TruncateAmount {
     private void start() {
 
         // TODO Use other methods you create to implement this program's functionality.
+        String original = getAmountFromUser();
+        int numDecimalPlaces = getDecimalPlacesFromUser();
+        printTruncatedAmount(original, numDecimalPlaces);
+
     }
 
     // TODO Write a method which prompts the user and reads the amount to truncate from the Keyboard
+    private String getAmountFromUser() {
+        System.out.println("Please enter an amount: ");
+        return Keyboard.readInput();
+    }
 
     // TODO Write a method which prompts the user and reads the number of DP's from the Keyboard
+    private int getDecimalPlacesFromUser() {
+        System.out.println("Please enter the number of decimal places: ");
+        return Integer.parseInt(Keyboard.readInput());
 
     // TODO Write a method which truncates the specified number to the specified number of DP's
+    private String truncate(String original, int numDecimalPlaces) {
+        int findDecimalIndex = original.indexOf(".");
+        return original.substring(0, findDecimalIndex + 1 + numDecimalPlaces);
+        }
 
     // TODO Write a method which prints the truncated amount
+    private void printTruncatedAmount(String original, int numDecimalPlaces) {
+            System.out.println("Amount truncated to " + numDecimalPlaces + " decimal places is: " + truncate(original, numDecimalPlaces));
+        }
 
     /**
      * Program entry point. Do not edit.
